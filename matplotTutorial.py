d7c6a3ab261770677f6c27edae0fc548df72046b
DavidNovo <david.novogrodsky@gmail.com>
2014-12-30 22:18:59 -0600

    removing the pre-defined style and manually adding style elements

--- matplotTutorial.py
+++ matplotTutorial.py
@@ -4,7 +4,7 @@ from matplotlib import pyplot as plt
 from matplotlib import style
 
 # adding a predefined style
-# style.use('dark_background')
+# style.use('ggplot')
 
 x = [0,2,7,8]
 y = [0,3,5,9]
@@ -15,7 +15,7 @@ y2 = [0,2,4,6]
 print(len(x))
 print(len(y))
 
-plt.plot(x,y,'b')
+plt.plot(x,y,'b',linewidth=5)
 plt.plot(x2,y2,'g')
 
 # adding titles and labels
