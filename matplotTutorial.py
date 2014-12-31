c4d4dea14599cf033c4dfd55174d19306ef90916
DavidNovo <david.novogrodsky@gmail.com>
2014-12-30 22:21:58 -0600

    added legend for the legend, it appears in the upper right corner of the graph.

--- matplotTutorial.py
+++ matplotTutorial.py
@@ -4,7 +4,7 @@ from matplotlib import pyplot as plt
 from matplotlib import style
 
 # adding a predefined style
-# style.use('ggplot')
+style.use('ggplot')
 
 x = [0,2,7,8]
 y = [0,3,5,9]
@@ -15,11 +15,14 @@ y2 = [0,2,4,6]
 print(len(x))
 print(len(y))
 
-plt.plot(x,y,'b',linewidth=5)
-plt.plot(x2,y2,'g')
+# adding a legend
+plt.plot(x,y,'b',linewidth=5,label='line 1')
+plt.plot(x2,y2,'g', label='line 2')
 
 # adding titles and labels
 plt.title("Epic Chart")
 plt.ylabel('y-axis')
 plt.xlabel('x-label')
+# making the legend show
+plt.legend()
 plt.show()
