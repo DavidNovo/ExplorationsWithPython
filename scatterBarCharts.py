35d48fe3706cb16f89f858c79ebfb64b2203364e
DavidNovo <david.novogrodsky@gmail.com>
2014-12-31 23:02:05 -0600

    experiments with scatter and bar charts

---
+++ scatterBarCharts.py
@@ -0,0 +1,28 @@
+__author__ = 'davidnovogrodsky_wrk'
+__author__ = 'davidnovogrodsky_wrk'
+
+from matplotlib import pyplot as plt
+from matplotlib import style
+
+# adding a predefined style
+style.use('ggplot')
+
+x = [0,2,7,8]
+y = [0,3,5,9]
+
+x2 = [0,4,6,9]
+y2 = [0,2,4,6]
+
+print(len(x))
+print(len(y))
+
+# adding a legend
+plt.plot(x,y,'b',linewidth=5,label='line 1')
+plt.plot(x2,y2,'g', label='line 2')
+
+# adding titles and labels
+plt.title("Epic Chart")
+plt.ylabel('y-axis')
+plt.xlabel('x-label')
+
+plt.show()
