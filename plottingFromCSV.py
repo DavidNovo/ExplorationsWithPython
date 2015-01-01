225addc181399eb60eac8a1cb3fed334dd42ad15
DavidNovo <david.novogrodsky@gmail.com>
2014-12-31 23:30:35 -0600

    using csv ffor matplotlib data points

---
+++ plottingFromCSV.py
@@ -0,0 +1,23 @@
+__author__ = 'davidnovogrodsky_wrk'
+__author__ = 'davidnovogrodsky_wrk'
+
+from matplotlib import pyplot as plt
+from matplotlib import style
+import numpy as np
+
+
+# adding a predefined style
+style.use('ggplot')
+
+#open the csv file
+x,y = np.loadtxt('dataPoints.txt',unpack=True,
+                 delimiter=',')
+
+plt.plot(x,y)
+
+# adding titles and labels
+plt.title("Epic Chart")
+plt.ylabel('y-axis')
+plt.xlabel('x-axis')
+
+plt.show()
