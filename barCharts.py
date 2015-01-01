423c5c54657bc15fc670515892efc2f3a208b86b
DavidNovo <david.novogrodsky@gmail.com>
2014-12-31 23:14:39 -0600

    more with bar chart, adding color and centering the bars over the x value.

--- barCharts.py
+++ barCharts.py
@@ -7,22 +7,23 @@ from matplotlib import style
 # adding a predefined style
 style.use('ggplot')
 
-x = [0,2,7,8]
-y = [0,3,5,9]
+x = [0,1,7,8]
+y = [2,3,5,9]
 
-x2 = [0,4,6,9]
-y2 = [0,2,4,6]
+x2 = [2,4,6,9]
+y2 = [1,2,4,6]
 
 print(len(x))
 print(len(y))
 
 # note color parameters different than in line graph
-plt.bar(x,y,color='c')
-plt.bar(x2,y2,color='k')
+# bars are not rendered in different colors by default
+plt.bar(x,y,color='c', align='center')
+plt.bar(x2,y2,color='k', align='center')
 
 # adding titles and labels
 plt.title("Epic Chart")
 plt.ylabel('y-axis')
-plt.xlabel('x-label')
+plt.xlabel('x-axis')
 
 plt.show()
