5fdd1f716ceae73610986d5ec259512f8e517d2f
DavidNovo <david.novogrodsky@gmail.com>
2015-01-01 22:55:02 -0600

    adding check against exit code of sub process

--- SubProcess.py
+++ SubProcess.py
@@ -2,9 +2,14 @@ __author__ = 'davidnovogrodsky_wrk'
 # the os module allows Python to talk to OS
 # subprocess allows shell to talk to Python
 import subprocess
-subprocess.call('ls',shell=True)
+
+# good idea to store the result code from the process
+# useful for flow control
+resultCode = subprocess.call('ls',shell=True)
 
 ## this stores the value from the OS
 output = subprocess.check_output('ls',shell=True)
 print(output)
+print("\n\n\n")
+print(resultCode)
 
