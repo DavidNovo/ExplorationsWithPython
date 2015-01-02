5ddc0796a8809a97ae8fe2fa9f759588301837d6
DavidNovo <david.novogrodsky@gmail.com>
2015-01-01 23:48:17 -0600

    made a connection use 'telnet localhost 5555' to test this

---
+++ socketBinding.py
@@ -0,0 +1,23 @@
+__author__ = 'davidnovogrodsky_wrk'
+
+import socket
+import sys
+
+# TCP socket with IP address
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = ''
+port = 5555
+
+try:
+    s.bind((host,port))
+except socket.error as e:
+    print(str(e))
+
+# number of connections to allow
+s.listen(5)
+conn, addr = s.accept()
+
+print('connected to:' +addr[0]+':'+str(addr[1]))
+
+
+
