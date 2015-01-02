a317d3f3e2b0f6fb6dd3c1404680f877fdf5ec67
DavidNovo <david.novogrodsky@gmail.com>
2015-01-01 22:56:16 -0600

    changed name of variable to exitCode for more unix flavor

--- SubProcess.py
+++ SubProcess.py
@@ -5,11 +5,11 @@ import subprocess
 
 # good idea to store the result code from the process
 # useful for flow control
-resultCode = subprocess.call('ls',shell=True)
+exitCode = subprocess.call('ls',shell=True)
 
 ## this stores the value from the OS
 output = subprocess.check_output('ls',shell=True)
 print(output)
 print("\n\n\n")
-print(resultCode)
+print('The exit code of the process is: ', exitCode)
 
