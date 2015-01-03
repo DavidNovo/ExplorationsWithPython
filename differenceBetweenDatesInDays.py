18683427c660914547b6b044ff292195524c44d4
DavidNovo <david.novogrodsky@gmail.com>
2015-01-03 12:06:28 -0600

    new assertion works

--- differenceBetweenDatesInDays.py
+++ differenceBetweenDatesInDays.py
@@ -38,9 +38,10 @@ def daysBetweenDates(year1, month1, day1, year2, month2, day2):
        and year2/month2/day2. Assumes inputs are valid dates
        in Gregorian calendar."""
     # program defensively! Add an assertion if the input is not valid!
-
+    assert not dateIsBefore(year2, month2, day2,year1, month1, day1)
     days = 0
     while dateIsBefore(year1, month1, day1, year2, month2, day2):
+
         year1, month1, day1 = nextDay(year1, month1, day1)
         days += 1
     return days
